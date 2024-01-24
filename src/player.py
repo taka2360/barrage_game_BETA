dc6d5276f66a59c76b8b56c35e923389b463c2f5
taka2360 <132958770+taka2360@users.noreply.github.com>
2024-01-24 13:13:47 +0900

    Update player.py

--- src/player.py
+++ src/player.py
@@ -2,9 +2,11 @@ import logging
 import tkinter as tk
 
 class Player:
-  def __init__(self, logger, canvas:tk.Canvas, x, y, character):
-    self.logger = logger
-    self.canvas = canvas
-    self.x = x
-    self.y = y
+    def __init__(self, logger, canvas:tk.Canvas, x, y, character):
+        self.logger = logger
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        match (character):
+        
     
