ce5836d99fa8747a62e0fce49390c304246c6561
taka2360 <132958770+taka2360@users.noreply.github.com>
2024-01-24 13:20:14 +0900

    Update player.py

--- src/player.py
+++ src/player.py
@@ -2,11 +2,33 @@ import logging
 import tkinter as tk
 
 class Player:
-    def __init__(self, logger, canvas:tk.Canvas, x, y, character):
+    def __init__(self, logger, canvas:tk.Canvas, x, y, speed, character):
         self.logger = logger
         self.canvas = canvas
         self.x = x
         self.y = y
-        match (character):
+        self.speed = speed
+        match character:
+            case "reimu":
+                #画像取得
+                pass
+        #仮
+        self.canvas.create
+    
+
+    def update(self):
+        pass
+
+    def move(self, dir):
+        match dir:
+            case 'u':
+                self.y -= self.speed
+            case 'd':
+                self.y += self.speed
+            case 'r':
+                self.x += self.speed
+            case 'l':
+                self.x -= self.speed
+
         
     
